0abba1a830c6fb6721551b84b0a89cfe6665fb44
Sarfaraz <m.s.hussain@outlook.com>
2021-09-16 12:50:01 -0500

    initialized react app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NavHeader from './components/NavHeader/NavHeader'
+import Contact from './components/Contact/Contact'
+
+function App() {
+  return (
+    <div className="">
+      <NavHeader/>
+      <Contact/>
+    </div>
+  );
+}
+
+export default App;
