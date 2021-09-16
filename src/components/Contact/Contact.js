22e30b23fd65b3397ea7615b55d25e9f22e4e748
Sarfaraz <m.s.hussain@outlook.com>
2021-09-16 12:51:54 -0500

    created src/components/contact

---
+++ src/components/Contact/Contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Contact() {
+    return (
+        <div class="footer">
+  <section id="contact me">
+  <h2>Contact me</h2>
+  <p> m.s.hussain@outlook.com</p>
+  <p>(512) 605 - 9034</p>
+  <a id="gh" href="https://github.com/MSarfarazH"><p>My GitHub </p> </a>
+  <a id="li" href="https://www.linkedin.com/in/msarfarazh/"><p>My LinkedIn </p> </a>
+  <a id="resume" href="https://zety.com/profile/msh-resume"><p>My Resume </p> </a>
+    </section>
+</div>
+
+    )
+}
+
+export default Contact
