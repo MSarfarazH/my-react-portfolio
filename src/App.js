c63a6a18621df89da224aea16381c26d0a86ec4c
Sarfaraz <m.s.hussain@outlook.com>
2021-09-20 18:39:37 -0500

    added components/projects

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import NavHeader from './components/NavHeader/NavHeader'
 import Contact from './components/Contact/Contact'
+import Projects from './components/Projects/Projects'
 
 function App() {
   return (
     <div className="">
       <NavHeader/>
+      <Projects/>
       <Contact/>
     </div>
   );
